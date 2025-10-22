79a10307155cfb679dbbf8bf0893435218ff4865
yuval-krn <yk2917@columbia.edu>
2025-10-21 23:54:32 -0400

    init commit

---
+++ 0/src/app.ts
@@ -0,0 +1,11 @@
+import * as net from "net";
+
+function newConn(socket: net.Socket): void {
+  socket.on("data", (data: Buffer) => {
+    socket.write(data);
+    socket.end();
+  });
+}
+const server = net.createServer();
+server.on("connection", newConn);
+server.listen({ host: "127.0.0.1", port: 1234 });
